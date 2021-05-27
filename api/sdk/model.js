a5d56843b56f37a8f9f977f4490dbb67b77bc890
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-05-27 10:16:05 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/trade_dnn/main/public/model_test/model.json';
+        const path = 'https://raw.githubusercontent.com/Islahuddin41420110058/jst-islahuddin-bismillah/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
