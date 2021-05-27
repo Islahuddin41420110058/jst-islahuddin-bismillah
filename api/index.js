6b95021c5ce9455016b5a748fbc32cb7cbd4cd4a
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-05-27 09:13:09 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,6 +28,42 @@ bot.onText(/\/menu/, (msg) => {
     );   
 });
 
+state = 0
+bot.onText(/\/predict/, (msg) => { 
+    console.log(msg)
+    bot.sendMessage(
+        msg.chat.id,
+        `masukan nilai i|v contohnya 9|9`
+    );   
+    state = 1;
+});
+
+bot.on('message', (msg) => {
+    if(state == 1){
+        console.log(msg.text);
+        s = msg.text.split("|");
+        i = s [0]
+        v = s [1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            console.log(jres);
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang diprediksi adalah ${jres [0]} volt `
+            ); 
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres [1]} watt `
+            );
+        })
+    }else{
+        state = 0
+    }
+})
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
@@ -41,3 +77,4 @@ r.get('/prediction/:i/:r', function(req, res, next) {
 });
 
 module.exports = r;
+
